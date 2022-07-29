ad5f3c4072cfbf3e6b36466f2f53aca947045191
Ashish Bhatnagar <ashishbhatnagar.cp@gmail.com>
2022-07-29 21:45:44 +0530

    init

---
+++ src/main/java/com/example/vertx/SimpleServerVerticle.java
@@ -0,0 +1,23 @@
+package com.example.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+
+public class SimpleServerVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> future) {
+        vertx.createHttpServer()
+                .requestHandler(
+                  r -> r.response().end("Welcome to Vert.x Intro"))
+                .listen(config().getInteger("http.port", 8080), result -> {
+                    if (result.succeeded()) {
+                        future.complete();
+                    } else {
+                        future.fail(result.cause());
+                    }
+                });
+    }
+
+}
